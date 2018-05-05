467672e60ee170f9eb1359f74d9bea6a6a2e70ea
biponbdkhu <biponbdkhu@gmail.com>
2018-05-05 10:56:19 +0600

    multiplication added

---
+++ 05-loop/js/while.js
@@ -0,0 +1,23 @@
+/*
+var i = 0;
+while(true){
+    console.log(i);
+    if(10 == i){
+        break;
+    }
+    i++;
+}
+
+*/
+
+var i = 0;
+while(i++ < 5){
+    console.log(i);
+}
+
+console.log("\n reset \n");
+
+var i = 0;
+while(++i < 5){
+    console.log(i);
+}
